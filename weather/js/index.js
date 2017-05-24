626bab53d045a63de99070de062fd40836fb5e74
dravo1 <a18645026693@163.com>
2017-05-24 18:55:23 +0800

    address change

--- weather/js/index.js
+++ weather/js/index.js
@@ -31,7 +31,7 @@
 	btn.onclick=function(){
 		value=input.value;
 		jsonp({
-			url:'http://wthrcdn.etouch.cn/weather_mini',
+			url:'https://wthrcdn.etouch.cn/weather_mini',
 			data:{
 				city:encodeURI(value)
 			},
